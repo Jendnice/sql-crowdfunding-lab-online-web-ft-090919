3c95fe8c6d76d43c591c27ef674466778418eb15
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-16 18:22:23 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,12 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "SELECT project.category, project.names, SUM(pledges.amount)
+  "SELECT project.category, users.name, SUM(pledges.amount)
+  FROM projects 
+  JOIN users 
+  ON projects.id
+  
+  WHERE project.category = music
   "
 end
 
