b26edb9e56820e01d2570a325e59c97fa33e87be
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-16 18:26:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -42,7 +42,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   FROM pledges
   JOIN users 
   ON pledges.user_id = users.id 
-  INNER JOIN projects 
+  JOIN projects 
   ON pledges.project_id = projects.id 
   WHERE projects.category = 'music';"
 end
