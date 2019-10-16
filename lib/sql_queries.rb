1549435899b7a2518961fde7a830905118e70d91
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-16 18:31:28 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -39,8 +39,8 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, pledges.amount
-  FROM pledges
-  JOIN projects 
+  FROM projects
+  JOIN pledges
   ON pledges.project_id = projects.id 
   WHERE projects.category = 'music'
   GROUP BY (projects.category);"
