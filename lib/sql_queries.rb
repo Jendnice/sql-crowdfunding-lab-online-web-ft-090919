a0ae796326a1a64bc75e2a16a3e37b5280bee624
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-16 18:08:52 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "SELECT projects.title, SUM(pledges.amount - projects.funding_goal) FROM projects
   JOIN pledges 
   ON projects.id = pledges.project_id 
-  WHERE pledges.amount > projects.funding_goal
+  HAVING pledges.amount > projects.funding_goal
   GROUP BY(projects.id);"
 end
 
